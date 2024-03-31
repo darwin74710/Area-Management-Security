ab439550f723ec96af6bc07a68568e45a99fbba2
Darwin Portela <darwin74710@gmail.com>
2024-03-31 12:04:19 -0500

    Se crea un poco de la interfaz de la ventana configuraciones.

--- src/main/java/Ventanas/Configuraciones.java
+++ src/main/java/Ventanas/Configuraciones.java
@@ -16,11 +16,14 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 import javax.swing.border.EmptyBorder;
 
 public class Configuraciones extends JFrame{
     public JLabel fondo = new JLabel();
+    JPanel elementos = new JPanel();
+    
     public JFrame ventanaAnterior = new JFrame();
     
     public Configuraciones(){
@@ -37,55 +40,131 @@ public class Configuraciones extends JFrame{
 
         fondo = new JLabel(new ImageIcon("Imagenes/fondo.png"));
         fondo.setLayout(null);
-        
-        //Establecemos imagen de fondo.
-        JScrollPane scrollFondo = new JScrollPane(fondo);
-        scrollFondo.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        scrollFondo.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        scrollFondo.setOpaque(true);
-        scrollFondo.setBorder(null);
-        this.add(scrollFondo);
-        
-        
-        Elementos();
-    }
-    private void Elementos(){
-        JPanel elementos = new JPanel();
-        elementos.setLayout(null);
-        elementos.setBounds(10,10,965,540);
-        elementos.setOpaque(false);
-        fondo.add(elementos);
+        this.add(fondo);
         
         //Creamos el boton para volver.
         JButton botonVolver = new JButton();
-        botonVolver.setBounds(890, 20, 50, 50);
+        botonVolver.setBounds(905, 15, 50, 50);
         botonVolver.setFocusPainted(false);
 
         ImageIcon logoInicio = new ImageIcon("Imagenes/Iconos/casa.png");
         botonVolver.setBackground(Color.decode("#000e3c"));
         botonVolver.setIcon(new ImageIcon(logoInicio.getImage().getScaledInstance(40, 40, Image.SCALE_SMOOTH))); //Redimencionamos la imagen para darle tamaño al boton.
-        elementos.add(botonVolver);
-        
-        JLabel tituloRegistro = new JLabel("Configuraciones Usuario");
-        tituloRegistro.setForeground(Color.white);
-        tituloRegistro.setFont(new Font("Arial",1,40));
-        tituloRegistro.setBounds(10, 15, 500, 50);
-        elementos.add(tituloRegistro);
-        
+        fondo.add(botonVolver);
         // Metodo para volver al menu
-        ActionListener irCamaras = new ActionListener() {
+        ActionListener irMenu = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 ventanaAnterior.setVisible(true);
                 dispose();
             }
         };
-        botonVolver.addActionListener(irCamaras);
+        botonVolver.addActionListener(irMenu);
         
-        JPanel configUsuarui = new JPanel();
-        configUsuarui.setBackground(Color.decode("#000a45"));
-        configUsuarui.setLayout(null);
-        configUsuarui.setBounds(10, 80, 925, 470);
-        elementos.add(configUsuarui);
+        Elementos();
+    }
+    private void Elementos(){
+        elementos.setBackground(Color.decode("#011b5a"));
+        elementos.setLayout(new BoxLayout(elementos, BoxLayout.Y_AXIS));
+        
+        //Establecemos imagen de fondo.
+        JScrollPane scrollFondo = new JScrollPane(elementos);
+        scrollFondo.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollFondo.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollFondo.setBounds(30,75,930,460);
+        scrollFondo.setOpaque(true);
+        scrollFondo.setBorder(null);
+        fondo.add(scrollFondo);
+        
+        JLabel tituloUsuario = new JLabel("CONFIGURACIÓN USUARIO");
+        tituloUsuario.setForeground(Color.white);
+        tituloUsuario.setFont(new Font("Arial",1,40));
+        tituloUsuario.setBounds(10, 15, 600, 50);
+        fondo.add(tituloUsuario);
+        
+        ConfigUsuario();
+        //ConfigAdmin();
+    }
+    
+    private void ConfigUsuario(){
+        JPanel fondoConfigUsuario = new JPanel();
+        fondoConfigUsuario.setOpaque(false);
+        fondoConfigUsuario.setLayout(null);
+        fondoConfigUsuario.setPreferredSize(new Dimension(1825, 450));
+        fondoConfigUsuario.setMaximumSize(new Dimension(1825, 450));
+        elementos.add(fondoConfigUsuario);
+        
+        JPanel configUsuario = new JPanel();
+        configUsuario.setBackground(Color.decode("#121A2D"));
+        configUsuario.setLayout(null);
+        configUsuario.setBounds(10, 10, 890, 440);
+        fondoConfigUsuario.add(configUsuario);
+        
+        JPanel temasApp = new JPanel();
+        temasApp.setLayout(null);
+        temasApp.setBackground(Color.decode("#000a45"));
+        temasApp.setBounds(10, 10, 430, 420);
+        configUsuario.add(temasApp);
+        
+        JLabel tituloTemas = new JLabel("TEMAS");
+        tituloTemas.setForeground(Color.white);
+        tituloTemas.setFont(new Font("Arial",1,20));
+        tituloTemas.setHorizontalAlignment(SwingConstants.CENTER);
+        tituloTemas.setBounds(10, 10, 410, 25);
+        temasApp.add(tituloTemas);
+        
+        JPanel modificarPerfil = new JPanel();
+        modificarPerfil.setLayout(null);
+        modificarPerfil.setBackground(Color.decode("#000a45"));
+        modificarPerfil.setBounds(450, 10, 430, 420);
+        configUsuario.add(modificarPerfil);
+        
+        JLabel tituloModificar = new JLabel("MODIFICAR PERFIL");
+        tituloModificar.setForeground(Color.white);
+        tituloModificar.setFont(new Font("Arial",1,20));
+        tituloModificar.setHorizontalAlignment(SwingConstants.CENTER);
+        tituloModificar.setBounds(10, 10, 410, 25);
+        modificarPerfil.add(tituloModificar);
+    }
+    
+    private void ConfigAdmin(){
+        JPanel fondoConfigAdmin = new JPanel();
+        fondoConfigAdmin.setOpaque(false);
+        fondoConfigAdmin.setLayout(null);
+        fondoConfigAdmin.setPreferredSize(new Dimension(1825, 450));
+        fondoConfigAdmin.setMaximumSize(new Dimension(1825, 450));
+        elementos.add(fondoConfigAdmin);
+        
+        JPanel configAdmin = new JPanel();
+        configAdmin.setBackground(Color.decode("#121A2D"));
+        configAdmin.setLayout(null);
+        configAdmin.setBounds(10, 10, 890, 440);
+        fondoConfigAdmin.add(configAdmin);
+        
+        JPanel Propiedades = new JPanel();
+        Propiedades.setLayout(null);
+        Propiedades.setBackground(Color.decode("#000a45"));
+        Propiedades.setBounds(10, 10, 430, 420);
+        configAdmin.add(Propiedades);
+        
+        JLabel tituloPropiedades = new JLabel("PROPIEDADES");
+        tituloPropiedades.setForeground(Color.white);
+        tituloPropiedades.setFont(new Font("Arial",1,20));
+        tituloPropiedades.setHorizontalAlignment(SwingConstants.CENTER);
+        tituloPropiedades.setBounds(10, 10, 410, 25);
+        Propiedades.add(tituloPropiedades);
+        
+        JPanel editarUsuarios = new JPanel();
+        editarUsuarios.setLayout(null);
+        editarUsuarios.setBackground(Color.decode("#000a45"));
+        editarUsuarios.setBounds(450, 10, 430, 420);
+        configAdmin.add(editarUsuarios);
+        
+        JLabel tituloEditar = new JLabel("EDITAR USUARIOS");
+        tituloEditar.setForeground(Color.white);
+        tituloEditar.setFont(new Font("Arial",1,20));
+        tituloEditar.setHorizontalAlignment(SwingConstants.CENTER);
+        tituloEditar.setBounds(10, 10, 410, 25);
+        editarUsuarios.add(tituloEditar);
     }
 }
